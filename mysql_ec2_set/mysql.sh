453f8d7ec45eb216bec07a8e3cd75abcca720f2c
cloudcoke <lms4296@gmail.com>
2023-02-19 12:55:40 +0900

    feat: switch 문 추가

--- mysql_ec2_set/mysql.sh
+++ mysql_ec2_set/mysql.sh
@@ -4,55 +4,82 @@ sudo DEBIAN_FRONTEND=noninteractive apt upgrade -y
 sudo DEBIAN_FRONTEND=noninteractive apt install mysql-server -y
 sudo systemctl start mysql
 
-while :;do 
-echo -n "Enter the password of the Mysql root account to use : "
-read -s PW
-echo
-echo -n "Re-enter your password : " 
-read -s RPW
-echo
+echo "Setting MySQL Root Account"
 
-if [ "$PW" == "$RPW" ]; then
-    break
-else 
-    echo "Passwords do not match."
-fi
+while :; do
+    echo -n "Enter the password of the MySQL root account to use : "
+    read -s PW
+    echo
+    echo -n "Re-enter your MySQL root account password : "
+    read -s RPW
+    echo
+
+    if [ $PW == $RPW ]; then
+        break
+    else
+        echo "Passwords do not match."
+    fi
 done
 
 sudo mysql -u root -e "alter user 'root'@'localhost' identified with mysql_native_password by '$PW'"
 sleep 1
-echo 
-echo "Complete Mysql root Password set"
+echo
+echo "Complete root Password"
 echo
 
-read -p "Enter the create Mysql user name : " USERNAME
-while :;do 
-echo -n "Enter the password of the $USERNAME account to use : "
-read -s USERPW
-echo
-echo -n "Re-enter your password : " 
-read -s USERRPW
-echo
+read -p "Do you want to create a MySQL user account : [y/n] " ANSWER
+case $ANSWER in
+[Yy]*)
+    read -p "Enter the create user name : " USERNAME
+    while :; do
+        echo -n "Enter the password of the $USERNAME account to use : "
+        read -s USERPW
+        echo
+        echo -n "Re-enter your password : "
+        read -s USERRPW
+        echo
 
-if [ "$USERPW" == "$USERRPW" ]; then
-    break
-else 
-    echo "Passwords do not match."
-fi
-done
+        if [ $USERPW == $USERRPW ]; then
+            break
+        else
+            echo "Passwords do not match."
+        fi
+    done
 
-read -p "Enter the create Mysql database name : " DBNAME
-
-sudo mysql -u root -p$PW <<QUERY
-create user '$USERNAME'@'%' identified with mysql_native_password by '$USERPW';
-grant all privileges on *.* to '$USERNAME'@'%' with grant option;
-create database $DBNAME
+    read -p "Do you want to allow external access : [y/n] " ALLOWACCESS
+    case $ALLOWACCESS in
+    [Yy]*)
+        SETIP='%'
+        sudo sed -i '0,/bind-address/{s/bind-address.*/bind-address = 0.0.0.0/}' /etc/mysql/mysql.conf.d/mysqld.cnf
+        ;;
+    [Nn]*)
+        SETIP=localhost
+        ;;
+    esac
+    sudo mysql -u root -p$PW <<QUERY
+            create user '$USERNAME'@'$SETIP' identified with mysql_native_password by '$USERPW';
+            grant all privileges on *.* to '$USERNAME'@'$SETIP' with grant option;
 QUERY
+    ;;
 
-sleep 2
-echo
-echo "Complete create user and database"
-sudo sed -i '0,/bind-address/{s/bind-address.*/bind-address = 0.0.0.0/}' /etc/mysql/mysql.conf.d/mysqld.cnf
+[Nn]*) ;;
+
+esac
+
+read -p "Do you want to create database : [y/n] " DBANSWER
+case $DBANSWER in
+[Yy]*)
+    read -p "Enter the create database name : " DBNAME
+    sudo mysql -u root -p$PW <<SETTING
+            create database $DBNAME
+SETTING
+    sleep 1
+    echo
+    echo "Complete create database"
+    ;;
+[Nn]*) ;;
+
+esac
+
 sudo systemctl restart mysql
-sudo iptables -A PREROUTING -t nat -i eth0 -p tcp --dport 80 -j REDIRECT --to-port 3000
-echo "Setup is complete"
+echo "Setting is complete"
